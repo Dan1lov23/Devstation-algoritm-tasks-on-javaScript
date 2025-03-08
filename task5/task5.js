4d54fed700e962bebe7a800f47a9934cbc190cc0
kitik <svy1dan@gmail.com>
2025-03-08 14:18:42 +0300

    task 5

--- task5/task5.js
+++ task5/task5.js
@@ -1,34 +1,15 @@
-// Нужно написать функцию squareOrSquareRoot(array), которая будет получать целочисленный массив в
-// качестве параметра и обрабатывать каждое число из этого массива.
-// Если число является полным квадратом, извлеките его корень, иначе возведите число в квадрат.
-// Функция должна вернуть новый массив, с обработанными числами полученного массива.
+// Дано натуральное число number.
+// Нужно написать функцию sumDigits(number), которая будет считывать все цифры
+// числа number и возвращать их сумму.
+// Возвращаемое значение должно быть числом.
 
-function task5(array) {
-    function isSimpleCheck(num) {
-        for (let x = 2; x < num; x++) {
-            if (num % x === 0) {
-                return 0;
-            }
-        }
-        return 1;
+function sumDigits(number) {
+    let sum = 0;
+    let stringNumber = String(number);
+    for (let a = 0; a < stringNumber.length; a++) {
+        sum += Number(stringNumber[a]);
     }
-
-    function checkSquare(number) {
-        if (isSimpleCheck(number) === 1) {
-            return number * number;
-        }
-        for (let i = 0; i < number; i++) {
-            if (i * i === number) {
-                return i;
-            }
-        }
-    }
-
-    let resultArray = [];
-    for (let y = 0; y < array.length; y++) {
-        resultArray.push(checkSquare(array[y]));
-    }
-    return resultArray;
+    return sum;
 }
 
-console.log(task5([4, 3, 9, 7, 2, 1]));
+sumDigits(212);
