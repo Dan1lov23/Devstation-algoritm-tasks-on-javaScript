d0c9b5a188350a90f095363f76e32e57bed20706
kitik <svy1dan@gmail.com>
2025-03-15 15:58:39 +0300

    task35

--- task35/task35.js
+++ task35/task35.js
@@ -1,38 +1,33 @@
-// Тринадцатеричная система счисления использует цифры 0-9,
-// а также латинские буквы A (10), B (11) и С (12).
-// Реализуйте функцию B13toB10(s), которая принимает строку, содержащую тринадцатеричное число,
-// и возвращает полученное из этой строки число в десятичной системе счисления.
-// Если строка s не является валидным тринадцатеричным числом, функция должна вернуть null.
-// Обратите внимание, что буквы А-С могут быть как прописными, так и строчными.
-// Присутствие любого другого символа, включая пробелы и знак "+", делает строку невалидной.
-// Исключение: первый символ строки может содержать знак "-".
+// Дана строка, состоящая из больших и маленьких букв латинского алфавита.
+// Необходимо написать функцию longestPalindrome(s), которая принимает такую строку единственным параметром и вычисляет длину максимально возможного палиндрома, который можно составить из букв этой строки, и возвращает его длину.
+// Например, из символов строки "abccccdde" можно построить палиндром "ccdadcc", чья
+// длина равняется семи символам.
+// То-есть результат вызова longestPalindrome("abccccdde") будет 7.
 
-function check(string) {
-    let numArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
-    for (let i = 0; i < numArray.length; i++) {
-        if (Number(string) === numArray[i]) {
-            return Number(string);
+
+function longestPalindrome(s) {
+    const charCount = {};
+
+    for (const char of s) {
+        charCount[char] = (charCount[char] || 0) + 1;
+    }
+
+    let length = 0;
+    let hasOdd = false;
+
+    for (const count of Object.values(charCount)) {
+        length += Math.floor(count / 2) * 2;
+
+        if (count % 2 === 1) {
+            hasOdd = true;
         }
     }
-    if (string === "A") {
-        return 10;
-    } else if (string === "B") {
-        return 11;
-    } else if (string === "C") {
-        return 12;
-    } else {
-        return false;
+
+    if (hasOdd) {
+        length += 1;
     }
+
+    return length;
 }
 
-function task35(string) {
-    let result = 0;
-    let factor = string.length - 1;
-    for (let a = 0; a < string.length; a++) {
-        result += check(string[a]) * 13 ** factor;
-        factor--;
-    }
-    return result;
-}
-
-console.log(task35('BAC'));
+longestPalindrome("abccccdde")
