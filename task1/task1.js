ecd33bef133bb0de72a70076ea3d3206be8322ed
kitik <svy1dan@gmail.com>
2025-03-16 13:57:30 +0300

    task1

--- task1/task1.js
+++ task1/task1.js
@@ -1,10 +1,21 @@
-// Напишите функцию calculateCoins(amount), принимающую на вход сумму денег amount.
-// Функция должна возвращать минимальное количество монет
-// разных номиналов для набора определенной суммы денег.
-// Имеются монеты номиналом: 1, 2, 5, 10 рублей.
+// Палиндром — слово, предложение или последовательность символов,
+// которая абсолютно одинаково читается как в привычном направлении, так и в обратном.
+// К примеру, “Anna” — это палиндром, а “table” и “John” — нет.
+// Дана строка str. Нужно написать функцию isPalindrome(str), которая позволяет вернуть значение true,
+// если строка является палиндромом, и false — если нет.
+// Алгоритм не должен быть зависимым от регистра символов.
 
-function task1(sum) {
-    console.log(sum / sum, sum / 2, sum / 5, sum / 10);
+function isPalindrome(str) {
+    let reverseStr = str.split("").reverse().join("")
+    let strResult = "";
+    for (let b = 0; b < str.length; b++) {
+        strResult += str[b].toLowerCase();
+    }
+    let reverseStrResult = "";
+    for (let b = 0; b < reverseStr.length; b++) {
+        reverseStrResult += reverseStr[b].toLowerCase();
+    }
+    return strResult === reverseStrResult;
 }
 
-task1(100);
+isPalindrome("Шалаш");
